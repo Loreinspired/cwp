1ce195da5921acd13a95532ad063cf7d9c95c5b0
Loreinspired <48743518+Loreinspired@users.noreply.github.com>
2026-02-23 17:44:54 +0100

    Add HelmetProvider for SEO support
    Wrap the App component with HelmetProvider for SEO enhancements.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async'; // ← Added this import
 import App from './App';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        {/* Wrap the App to enable SEO document head modifications */}
+        <HelmetProvider>
+            <App />
+        </HelmetProvider>
     </React.StrictMode>
 );
